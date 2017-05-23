8d4ba7f8196b0781a2abba30f506b2321e0273b9
Gustavo Gard <gusgard@gmail.com>
2017-05-23 18:24:40 -0300

    Add catch decorator and yarn.lock

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export {IMiddleware} from './base/IMiddleware';
 
 // Decorators.
 export {AccessControl} from './decorators/AccessControl';
+export {Catch} from './decorators/Catch';
 export {Endpoint} from './decorators/Endpoint';
 export {Model} from './decorators/Model';
 export {GET} from './decorators/GET';
@@ -15,4 +16,3 @@ export {POST} from './decorators/POST';
 export {PUT} from './decorators/PUT';
 export {PATCH} from './decorators/PATCH';
 export {DELETE} from './decorators/DELETE';
-
